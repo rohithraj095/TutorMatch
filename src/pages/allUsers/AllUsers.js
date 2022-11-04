147db844ab14b5a8143df6d29cfba958f962d3dd
jamestang <tang485@purdue.edu>
2022-11-04 13:23:42 -0400

    Able to change ratings

--- src/pages/allUsers/AllUsers.js
+++ src/pages/allUsers/AllUsers.js
@@ -1,4 +1,8 @@
 import { useCollection } from '../../hooks/useCollection'
+import styles from '../home/Home.module.css'
+import AddRating from './AddRating'
+
+
 
 // styles
 import './AllUsers.css'
@@ -8,12 +12,14 @@ export default function AllUsers() {
 
   return (
     <div className="user-list">
-      <h2>Username&emsp;&emsp;Phone Number&emsp;&emsp;PersonalBio</h2>
+      <h2>Username&emsp;&emsp;Phone Number&emsp;&emsp;PersonalBio&emsp;&emsp;Rating&emsp;&emsp;Add Rating</h2>
       {isPending && <div>Loading users...</div>}
       {error && <div>{error}</div>}
       {documents && documents.map(user => (
         <div key={user.id} className="user-list-item">
-          <span>{user.name}&emsp;&emsp;&emsp;&emsp;&emsp;{user.cellNum}&emsp;&emsp;&emsp;&emsp;{user.personalBio}</span>
+          <span>{user.name}&emsp;&emsp;&emsp;&emsp;&emsp;{user.cellNum}&emsp;&emsp;&emsp;&emsp;{user.personalBio}&emsp;&emsp;&emsp;&emsp;{(user.totalRating)}&emsp;&emsp;&emsp;&emsp;
+          {<div className={styles.container}><AddRating uid={user.id}/></div>}
+          </span>
         </div>
       ))}
     </div>
