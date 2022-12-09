7517b8f1d82a62e8772750bb378977650d374a5d
jamestang <tang485@purdue.edu>
2022-12-09 13:54:22 -0500

    Hide Profile Basically Working

--- src/pages/home/ClassListNoDel.js
+++ src/pages/home/ClassListNoDel.js
@@ -1,19 +1,27 @@
 import {useFirestore} from '../../hooks/useFirestore'
+import {projectFirestore} from '../../firebase/config'
+import { useAuthContext } from '../../hooks/useAuthContext'
+
+
 
 // styles
 import styles from './Home.module.css'
 
 
 export default function ClassesListNoDel({ Classes }) {
+  const { user } = useAuthContext()
   return (
 
     <ul className={styles.course}>
       {Classes.map((classes) => (
+        //((projectFirestore.collection("users").doc(classes.uid).get().hideProfile==="false") || (projectFirestore.collection("users").doc(classes.uid).get().hideProfile==null)) ? (
+        //(users.hideProfile==="false" || users.hideProfile==null) ? (
         <li key={classes.id}>
         <p className={styles.name}>{classes.name}</p>
         <p className={styles.coursecode}>{classes.isTutor}</p>
         <p className={styles.username}>user: {classes.userName}</p>
       </li>
+        //) : null
       ))}
     </ul> 
   )
